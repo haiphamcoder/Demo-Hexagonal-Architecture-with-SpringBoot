91e6dd86aab73c269251757519bad93370a4a255
Hai Pham <ngochai285nd@gmail.com>
2023-10-30 15:51:14 +0700

    init project

---
+++ src/main/java/com/hexagonal/demo/DemoHexagonalArchitectureWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHexagonalArchitectureWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHexagonalArchitectureWithSpringBootApplication.class, args);
+    }
+
+}
